0069d5b2272848afcdd233d7610a01c7796a9fb9
seemadurrani <59551872+seemadurrani@users.noreply.github.com>
2021-07-16 11:52:02 +0530

    Update Repo-pr.py

--- Repo-pr.py
+++ Repo-pr.py
@@ -1,10 +1,18 @@
+# Usage $file_name <token> <repository-name>
+
+
 from github import Github
 import sys
-g = Github(sys.argv[1])
-repo = g.get_user().get_repo(sys.argv[2])
-contents = repo.get_contents("")
-for post in contents:
-    print(post.name)
-pulls = repo.get_pulls(state='open')
-for pr in pulls:
-    print(pr.number)
+try:
+   g = Github(sys.argv[1])
+   repo = g.get_user().get_repo(sys.argv[2])
+   contents = repo.get_contents("")
+   for post in contents:
+      print(post.name)
+   pulls = repo.get_pulls(state='open')
+   for pr in pulls:
+      print(pr.number)
+except IndexError:
+    print("Token and Repository name is not entered")
+except Exception as e:
+    print(e)
